c1e65f52b3d79db1ab5918f43596a937ccd7f230
Robert Kuramshin <robert@sensorscall.com>
2018-10-13 13:00:57 -0400

    adding route table size macro

--- main/config.h
+++ main/config.h
@@ -5,7 +5,7 @@
 #define MESH_VOTE_PERCENTAGE 0.9
 #define MESH_ASSOC_EXP 10
 #define MESH_CHANNEL    1
-
+#define MESH_ROUTE_TABLE_SIZE 50
 //---------------------------------------------------------------------------
 // ROUTER
 //---------------------------------------------------------------------------
